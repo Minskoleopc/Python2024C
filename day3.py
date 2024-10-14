187f66eb22ac8bf054cabdd3c21b15538b489993
unknown <Minskoleopc>
2024-10-14 21:51:52 +0530

    added

---
+++ day3.py
@@ -0,0 +1,92 @@
+#           0         1         2      3
+names = ["chinmay","sarika","poorva","ram"]
+print(names[0])
+
+# program 2
+# update 
+names[0] = "tanmay"
+
+#program 3 check whether element is present
+print("tanmay" in names)
+
+#program 4 total elements in list
+print(len(names))
+
+#program 5
+city = ["pune","mumbai","banglore","kolkata"]
+# for 
+
+for x in range(len(city)):
+    #print(x)
+    print(city[x])
+
+
+# while 
+city = ["pune","mumbai","banglore","kolkata"]
+j = 0
+while(j < len(city)):
+    #print(j)
+    print(city[j])
+    j = j + 1
+
+# for - basic
+city = ["pune","mumbai","banglore","kolkata"]
+for x in city:
+    print(x)
+
+
+# program 6
+# methods of list
+
+numbers = [11,22,33]
+# numbersB = numbers
+# numbers[0] = 111
+# print(numbers)
+# print(numbersB)
+
+
+# numbersC = numbers.copy()
+# numbersC[0] = 111
+# print(numbersC)
+# print(numbers)
+
+
+# program 2
+#           0       1       2
+fruits = ["apple","mango","banana"]
+# fruits.append('grapes')
+# print(fruits)
+
+# program 3
+#fruits.pop()
+#fruits.pop(1)
+#fruits.remove("apple")
+#print(fruits)
+
+# program 4
+fruits = ["apple","mango","banana"]
+# fruits.sort()
+# print(fruits)
+
+# fruits.extend(["payaya","grapes","orange"])
+# print(fruits)
+
+fruits = ["apple","mango","banana"]
+# e = fruits.index("apple")
+# print(e)
+
+# fruits.reverse()
+# print(fruits)
+
+# fruits.insert(2,"papaya")
+# print(fruits)
+
+# fruits.clear()
+# print(fruits)
+
+# del fruits
+# print(fruits)
+
+
+
+
