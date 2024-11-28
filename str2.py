a9658a21b13fd80cd55cf754b92e0b0fceab0ab8
unknown <Minskoleopc>
2024-11-28 21:54:26 +0530

    added

---
+++ str2.py
@@ -0,0 +1,60 @@
+# captialize
+fn = "chinmay"
+print(fn.capitalize())
+
+#upper()
+print(fn.upper())
+
+# lower()
+ln = "deshpande"
+print(ln.lower())
+
+#swapcase
+city = "Pune"
+print(city.swapcase())
+
+# title
+info = "I am learning js"
+print(info.title())
+
+# string alignment
+# center
+text = "hello"
+t = text.center(15,"-") # -----hello-----
+print(t)
+
+
+# ljust()
+text = "hello"
+t = text.ljust(15,"-") 
+print(t)
+
+# rjust()
+text = "hello"
+t = text.rjust(15,"-") 
+print(t)
+
+text = "42"
+t = text.zfill(10)
+print(t)
+
+
+# Substring
+h = "chinmaya" # string
+h = h.count('a')
+print(h)
+
+# find()
+
+text = "chinmay"
+r = text.find('A')
+print(r)
+
+text = "chinmaya"
+r = text.find('a')
+print(r)
+
+r = text.rfind('a')
+print(r)
+
+
